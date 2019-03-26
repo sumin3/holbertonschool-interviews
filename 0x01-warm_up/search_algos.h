8c03c72a2637cbb56f210b299782fb46333f2750
sumin3 <suminyu@yahoo.com>
2019-03-26 01:19:12 +0000

    init commit

---
+++ 0x01-warm_up/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int binary_search(int *array, size_t size, int value);
+
+
+
+
+#endif /* _BINARY_TREES_H_ */
